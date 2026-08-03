75eab300bbb9ee2f94e7178dc4b378ff12190541
dangtienvn <td2812009@gmail.com>
2026-08-03 17:17:58 +0700

    fix: resolve Next.js DYNAMIC_SERVER_USAGE build error in blog-cms admin pages

--- apps/blog-cms/app/admin/blogs/categories/page.js
+++ apps/blog-cms/app/admin/blogs/categories/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic';
 
 export default async function AdminCategoriesPage() {
     let categories = [];
--- apps/blog-cms/app/admin/blogs/page.js
+++ apps/blog-cms/app/admin/blogs/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
+export const dynamic = 'force-dynamic';
 
 export default async function AdminProductsPage() {
     // Gọi Database lấy danh sách bài viết admin
