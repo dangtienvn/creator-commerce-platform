ec8e71b108635de23884361c3d6a263a88dc9c65
dangtienvn <td2812009@gmail.com>
2026-08-16 15:25:00 +0700

    feat(storefront): add content section components

---
+++ apps/storefront/src/components/CommentSection.js
@@ -0,0 +1,3 @@
+﻿export default function CommentSection() {
+  return <div className="p-4 border rounded-lg shadow-sm mt-8">Comment Section Stub</div>;
+}
---
+++ apps/storefront/src/components/PostCard.js
@@ -0,0 +1,3 @@
+﻿export default function PostCard({ post }) {
+  return <div className="p-4 border rounded-lg shadow-sm">Post Card Stub</div>;
+}
